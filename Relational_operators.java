355c97d05d8680a52330d994da6050fb84190a10
Saurabh022mishra <skm80050@gmail.com>
2023-10-01 09:02:21 +0530

    Relational_Operators

---
+++ Relational_operators.java
@@ -0,0 +1,13 @@
+public class Relational_operators {
+    // Relational Operators
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+        System.out.println(a > b);//False
+        System.out.println(a < b);//True
+        System.out.println(a >= b);//False
+        System.out.println(a <= b);//True
+        System.out.println(a == b);//False
+        System.out.println(a != b);//False
+    }
+}
