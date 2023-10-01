34fe834d112172ac2f85e680816ceb1ebd1680bd
Saurabh022mishra <skm80050@gmail.com>
2023-10-01 09:14:49 +0530

    Assignment_Operators

---
+++ Assignment_Operators.java
@@ -0,0 +1,23 @@
+public class Assignment_Operators {
+    public static void main(String[] args){
+        int income=30000;
+        int bonus=5000;
+        //income=income+bonus
+        income+=bonus;
+        System.out.println(income);
+        //income=income-bonus
+        income-=bonus;
+        System.out.println(income);
+        //income=income*bonus
+        income*=bonus;
+        System.out.println(income);
+        //income=income/bonus
+        income/=bonus;
+        System.out.println(income);
+        //income=income%bonus
+        income%=bonus;
+        System.out.println(income);
+        
+
+    }
+}
