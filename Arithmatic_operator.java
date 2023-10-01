cc9f7feeaf44671b15e3335d73c056757f75db02
Saurabh022mishra <skm80050@gmail.com>
2023-10-01 08:30:26 +0530

    Arithmatic_operators

---
+++ Arithmatic_operator.java
@@ -0,0 +1,17 @@
+public class Arithmatic_operator {
+    public static void main(String[] args){
+        //Arithmatic Operators
+        int result=9%2;
+        System.out.println(result);
+        int plus=9+2;
+        System.out.println(plus);
+        int minus=9-2;
+        System.out.println(minus);
+        int multiply=9*2;
+        System.out.println(multiply);
+        int divide=9/2;
+        System.out.println(divide);
+        float divide1=9.0f/2;
+        System.out.println(divide1);
+    }
+}
