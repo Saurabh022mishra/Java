bba1548693c3849d81feaf2e670040337884c6f8
Saurabh022mishra <skm80050@gmail.com>
2023-10-06 23:03:36 +0530

    Conditionals_Statement

---
+++ If_Else.java
@@ -0,0 +1,15 @@
+public class If_Else {
+    public static void main(String[] args){
+        //IF_ELSE statement
+        boolean isSecular=true;
+        String msg;
+        if(isSecular){
+            msg="India is a secular country";
+        }
+        else{
+            msg="India is not a secular country";
+        }
+        System.out.println(msg);
+    }
+    
+}
