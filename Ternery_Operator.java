12a0dfd0a67d1972a99aa2b267a30a301382f139
Saurabh022mishra <skm80050@gmail.com>
2023-10-06 23:09:46 +0530

    Ternary_Operator

---
+++ Ternery_Operator.java
@@ -0,0 +1,11 @@
+public class Ternery_Operator {
+    public static void main(String[] args){
+        //TERNERY OPERATOR
+        //SYNTAX: (condition)?(true statement):(false statement)
+        boolean isSecular=true;
+        String msg;
+        msg=(isSecular)?"India is a secular country":"India is not a secular country";
+        System.out.println(msg);
+    }
+    
+}
