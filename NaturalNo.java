8c33d5d2c1583deee0bb7d97a3e5adc95c4d23ba
Saurabh022mishra <skm80050@gmail.com>
2023-10-14 14:13:22 +0530

    WhileLoop

---
+++ NaturalNo.java
@@ -0,0 +1,10 @@
+public class NaturalNo {
+    //WAP to find natural numbers from 1 to 100
+    public static void main(String[] args){
+        int a=1;
+        while(a<=100){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
