83a486768710cb65a163cc7584405bf12328cdee
Saurabh022mishra <skm80050@gmail.com>
2023-10-23 10:24:55 +0530

    Reversed Array

--- Array_2.java
+++ Array_2.java
@@ -1,10 +1,8 @@
+
 //Reversed Array
 import java.util.Scanner;
 
 public class Array_2 {
-    public Array_2() {
-    }
-
     public static void main(String[] args) {
         int j = 0;
         Scanner sc = new Scanner(System.in);
@@ -14,19 +12,20 @@ public class Array_2 {
         int[] rev = new int[n + 1];
 
         int i;
-        for(i = 0; i < n; ++i) {
+        for (i = 0; i < n; ++i) {
             System.out.println("Enter " + i + " th NO:");
             arr[i] = sc.nextInt();
         }
 
-        for(i = arr.length - 1; i >= 0; --i) {
+        for (i = arr.length - 1; i >= 0; --i) {
             rev[j] = arr[i];
             ++j;
         }
 
-        for(i = 0; i < rev.length - 1; ++i) {
+        for (i = 0; i < rev.length - 1; ++i) {
             System.out.print(rev[i] + " ");
         }
-
+        sc.close();
+        
     }
 }
