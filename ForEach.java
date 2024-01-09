0eccbd50b8eb2c719296efcbdc3e5d74fa7dca7b
Saurabh022mishra <skm80050@gmail.com>
2024-01-09 22:55:11 +0530

    ForEach Loop

---
+++ ForEach.java
@@ -0,0 +1,20 @@
+public class ForEach {
+
+        public static void main(String[] args) {
+        
+        /* FOR EACH Loop Practice */
+        
+        double[] arr=new double[3];
+        arr[0]=1212.34;
+        arr[1]=211313.34;
+        arr[2]=234234.67;
+        for(double a:arr){
+            System.out.println(a);
+        }                                                                                                                                                                          
+        
+        
+        
+        }
+        
+        }
+
